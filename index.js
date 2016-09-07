4106adb09fa83c4a3642d3da413681f78215c7ab
Connor Leech <connorleech@gmail.com>
2016-09-06 21:36:32 -0700

    init vue-webpack integration madness

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = require('express')(),
 	authors = require('./server/controllers/authors'),
 	books = require('./server/controllers/books');
 
+// express config
+app.set('port', process.env.PORT || 8000);
+
 // authors routes
 app.get('/authors', authors.index);
 app.get('/authors/:id', authors.show);
@@ -16,7 +19,6 @@ app.get('/books/:id', books.show);
 app.post('/books', books.create);
 app.delete('/books', books.delete);
 
-app.set('port', process.env.PORT || 8000);
 
 app.listen(app.get('port'), function () {
   console.log("Server started on port", app.get('port'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // the main entry of our app
+  entry: ['./src/index.js', './src/auth/index.js'],
+  // output configuration
+  output: {
+    path: __dirname + '/build/',
+    publicPath: 'build/',
+    filename: 'build.js'
+  },
+
+  module: {
+    loaders: [
+      // process *.vue files using vue-loader
+      { test: /\.vue$/, loader: 'vue' },
+      // process *.js files using babel-loader
+      // the exclude pattern is important so that we don't
+      // apply babel transform to all the dependencies!
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ }
+    ]
+  },
+
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  }
+}
