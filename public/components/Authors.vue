ca56de2e35ef2a36775c97562894813b8ad645db
Connor Leech <connorleech@gmail.com>
2016-09-27 22:29:36 -0700

    make an ajax call with vue-resource

--- public/components/Authors.vue
+++ public/components/Authors.vue
@@ -1,9 +1,10 @@
 
 <template>
 	<div class='col-sm-6 col-sm-offset-3'>
-		<h1>Authors request</h1>
+		<h1>Authors</h1>
 
-		<h4>Static arrays</h4>
+		<pre>{{authors | json}}</pre>
+		<h4>Static array of States that start with letter "A"</h4>
 		<typeahead :data="USstate" placeholder="USA states">
 		</typeahead>
 	</div>
@@ -11,17 +12,31 @@
 
 <script>
 import { typeahead } from 'vue-strap'
+
 export default {
 	components: {
 		typeahead
 	},
+	ready(){
+		this.getAuthors()
+
+	},
 	data(){
 		return {
+			authors: [],
 			USstate: ['Alabama', 'Alaska', 'Arizona'],
 		}
 	},
 	methods: {
-
+		getAuthors: function(){
+			console.log('get authors called')
+			this.$http.get('/api/authors').then(function(authors){
+				console.log(authors)
+                this.$set('authors', authors.body)
+            }, function(err){
+            	console.error('oh shit')
+            })
+		}
 	}
 }
 </script>
